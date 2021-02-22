6f3c93db2362af409bca23d1f85cef3116f6d122
Corban Joyce <corbantjoyce@gmail.com>
2021-02-21 19:36:34 -0500

    installed material ui and added accordion

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache, gql, useQuery } from '@apollo/client';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import styles from './styles.module.css';
 
 const client = new ApolloClient({
@@ -27,15 +32,19 @@ function Users() {
 
     return data.users.map((user, index) => (
         <div key={user.id}>
-            <p className={styles.name}>
-                {user.name}
-            </p>
-            <p className={styles.email}>
-                {user.email}
-            </p>
-            <p className={styles.phone}>
-                {user.phone}
-            </p>
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                >
+                    <Typography className={styles.name} >{user.name}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography className={styles.email} >Email: {user.email}</Typography>
+                    <Typography className={styles.phone} >Phone: {user.phone}</Typography>
+                </AccordionDetails>
+            </Accordion>
         </div>
     ));
 }
